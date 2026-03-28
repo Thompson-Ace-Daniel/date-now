32f1f7ab8d09c0cda2c181d30238d4b8c59dbdee
Thompson Daniel <thompsonacedaniel@gmail.com>
2026-03-28 11:46:34 +0100

    complete tab bar

--- constants/theme.ts
+++ constants/theme.ts
@@ -3,51 +3,55 @@
  * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
  */
 
-import { Platform } from 'react-native';
+import { Platform } from "react-native";
 
-const tintColorLight = '#0a7ea4';
-const tintColorDark = '#fff';
+const tintColorLight = "#FF4458";
+const tintColorDark = "#FF5864";
 
 export const Colors = {
   light: {
-    text: '#11181C',
-    background: '#fff',
+    text: "#1A1A1A",
+    background: "#FFFFFF",
     tint: tintColorLight,
-    icon: '#687076',
-    tabIconDefault: '#687076',
+    icon: "#7C7C7C",
+    tabIconDefault: "#ADADAD",
     tabIconSelected: tintColorLight,
+    border: "#F2F2F2",
   },
   dark: {
-    text: '#ECEDEE',
-    background: '#151718',
+    text: "#F5F5F5",
+    background: "#0F1112",
     tint: tintColorDark,
-    icon: '#9BA1A6',
-    tabIconDefault: '#9BA1A6',
+    icon: "#9BA1A6",
+    tabIconDefault: "#4E5458",
     tabIconSelected: tintColorDark,
+    border: "#26292B",
   },
 };
 
 export const Fonts = Platform.select({
   ios: {
-    /** iOS `UIFontDescriptorSystemDesignDefault` */
-    sans: 'system-ui',
-    /** iOS `UIFontDescriptorSystemDesignSerif` */
-    serif: 'ui-serif',
-    /** iOS `UIFontDescriptorSystemDesignRounded` */
-    rounded: 'ui-rounded',
-    /** iOS `UIFontDescriptorSystemDesignMonospaced` */
-    mono: 'ui-monospace',
+    sans: "System",
+    serif: "Times New Roman",
+    rounded: "ui-rounded",
+    mono: "Menlo",
   },
-  default: {
-    sans: 'normal',
-    serif: 'serif',
-    rounded: 'normal',
-    mono: 'monospace',
+  android: {
+    sans: "Times",
+    serif: "serif",
+    rounded: "sans-serif-medium",
+    mono: "monospace",
   },
   web: {
-    sans: "system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif",
+    sans: "Inter, system-ui, -apple-system, Segoe UI, Roboto, sans-serif",
     serif: "Georgia, 'Times New Roman', serif",
-    rounded: "'SF Pro Rounded', 'Hiragino Maru Gothic ProN', Meiryo, 'MS PGothic', sans-serif",
-    mono: "SFMono-Regular, Menlo, Monaco, Consolas, 'Liberation Mono', 'Courier New', monospace",
+    rounded: "Figtree, 'SF Pro Rounded', sans-serif",
+    mono: "ui-monospace, 'Cascadia Code', 'Segoe UI Mono', monospace",
+  },
+  default: {
+    sans: "System",
+    serif: "serif",
+    rounded: "System",
+    mono: "monospace",
   },
 });
